a472f8159312abdba435dcfd7f950085c4273020
badfalcon <badfalcon610@gmail.com>
2022-09-19 06:08:35 +0900

    add work images to works

--- src/Pages/Works.js
+++ src/Pages/Works.js
@@ -1,5 +1,6 @@
 import './Works.css';
 import {
+  Box,
   Card,
   CardActionArea,
   CardContent,
@@ -9,42 +10,42 @@ import {
 
 export const Works = () => {
   const works = [
-    {title:"Skin Editor", desc:"Java", src:""},
-    {title:"Skin Editor(Web)", desc:"JavaScript", src:""},
-    {title:"ImageDrawer", desc:"Chrome Ext", src:""},
-    {title:"Old Personal Website", desc:"JavaScript", src:""},
+    {title:"Skin Editor", desc:"Java", src:"http://i.imgur.com/FWmdTUk.png"},
+    {title:"Skin Editor(Web)", desc:"JavaScript", src:`${process.env.PUBLIC_URL}/images/web_skin_editor.png`},
+    {title:"Skin Poser", desc:"JavaScript", src:`${process.env.PUBLIC_URL}/images/skin_poser.png`},
+    {title:"ImageDrawer", desc:"Chrome Ext", src:`${process.env.PUBLIC_URL}/images/image_drawer.jpg`},
+    {title:"Old Personal Website", desc:"JavaScript", src:`${process.env.PUBLIC_URL}/images/old_website.png`},
   ]
   return <Container>
-    <h1>Works</h1>
+    <Box sx={{pt:10}}>
+    </Box>
     <Grid
         container
-        spacing={2} direction="row"
+        spacing={2}
         justifyContent="center"
         alignItems="center"
     >
       {works.map(v => (
           // todo review layout
-          <Grid item xs={5} sm={5} md={5}>
-            <Container>
-              <Card centered sx={{maxWidth: 345}}>
-                <CardActionArea>
-                  <CardMedia
-                      component="img"
-                      height="140"
-                      image={v.src}
-                      alt={v.title}
-                  />
-                  <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                      {v.title}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                      {v.desc}
-                    </Typography>
-                  </CardContent>
-                </CardActionArea>
-              </Card>
-            </Container>
+          <Grid item xs={11} sm={8} md={6}>
+            <Card centered>
+              <CardActionArea>
+                <CardMedia
+                    component="img"
+                    height="140"
+                    image={v.src}
+                    alt={v.title}
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {v.title}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {v.desc}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
           </Grid>
       ))}
     </Grid>
