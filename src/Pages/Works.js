6342d8ba590591e29092052432a491d1dda28aea
badfalcon <badfalcon610@gmail.com>
2022-09-24 04:31:01 +0900

    add key to list

--- src/Pages/Works.js
+++ src/Pages/Works.js
@@ -11,11 +11,11 @@ import {Trans} from "react-i18next";
 
 export const Works = () => {
   const works = [
-    {title:"Skin Editor", desc:"Java", src:"http://i.imgur.com/FWmdTUk.png"},
-    {title:"Skin Editor(Web)", desc:"JavaScript", src:`${process.env.PUBLIC_URL}/images/web_skin_editor.png`},
-    {title:"Skin Poser", desc:"JavaScript", src:`${process.env.PUBLIC_URL}/images/skin_poser.png`},
-    {title:"ImageDrawer", desc:"Chrome Ext", src:`${process.env.PUBLIC_URL}/images/image_drawer.jpg`},
-    {title:"Old Personal Website", desc:"JavaScript", src:`${process.env.PUBLIC_URL}/images/old_website.png`},
+    {key:0, title:"Skin Editor", desc:"Java", src:"http://i.imgur.com/FWmdTUk.png"},
+    {key:1, title:"Skin Editor(Web)", desc:"JavaScript", src:`${process.env.PUBLIC_URL}/images/web_skin_editor.png`},
+    {key:2, title:"Skin Poser", desc:"JavaScript", src:`${process.env.PUBLIC_URL}/images/skin_poser.png`},
+    {key:3, title:"ImageDrawer", desc:"Chrome Ext", src:`${process.env.PUBLIC_URL}/images/image_drawer.jpg`},
+    {key:4, title:"Old Personal Website", desc:"JavaScript", src:`${process.env.PUBLIC_URL}/images/old_website.png`},
   ]
   return <Container sx={{pt:10}}>
     <Box sx={{pb:5}}>
@@ -31,8 +31,8 @@ export const Works = () => {
     >
       {works.map(v => (
           // todo review layout
-          <Grid item xs={11} sm={8} md={6}>
-            <Card centered>
+          <Grid item xs={11} sm={8} md={6} key={v.key}>
+            <Card>
               <CardActionArea>
                 <CardMedia
                     component="img"
