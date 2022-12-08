51610f4e5c38bb92e67af06393a51ffce993cb5b
badfalcon <badfalcon610@gmail.com>
2022-12-09 03:13:20 +0900

    add pulldown menu for small screen sizes

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,6 +1,7 @@
 import {matchPath, NavLink, useLocation} from "react-router-dom";
 import {Box, Tab, Tabs} from "@mui/material";
 import Language from "./Language";
+import MiniMenu from "./MiniMenu";
 
 
 export const useRouteMatch = (patterns) => {
@@ -21,21 +22,26 @@ export const Menu = () => {
   const routeMatch = useRouteMatch(['/', '/about', '/works', '/contact']);
   const currentTab = routeMatch?.pattern?.path ?? false;
   return <header>
-    <nav>
-      <Box sx={{ display: 'flex', alignItems: 'center' }}>
-        <Box sx={{ flexGrow: 1}}>
-          <Tabs value={currentTab} aria-label="nav tabs" centered>
-            <Tab label="Home" value="/" to={"/"} component={NavLink} />
-            <Tab label="About" value="/about"  to={"/about"} component={NavLink} />
-            <Tab label="Works" value="/works"  to={"/works"} component={NavLink} />
-            <Tab label="Contact" value="/contact"  to={"/contact"} component={NavLink} />
-          </Tabs>
+    <Box sx={{ display: { xs:'none', sm: 'block'} }}>
+      <nav>
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          <Box sx={{ flexGrow: 1}}>
+            <Tabs value={currentTab} aria-label="nav tabs" centered >
+              <Tab label="Home" value="/" to={"/"} component={NavLink} />
+              <Tab label="About" value="/about"  to={"/about"} component={NavLink} />
+              <Tab label="Works" value="/works"  to={"/works"} component={NavLink} />
+              <Tab label="Contact" value="/contact"  to={"/contact"} component={NavLink} />
+            </Tabs>
+          </Box>
+          <Box>
+            <Language />
+          </Box>
         </Box>
-        <Box>
-          <Language />
-        </Box>
-      </Box>
-    </nav>
+      </nav>
+    </Box>
+    <Box sx={{ display: { xs:'block', sm: 'none'} }}>
+      <MiniMenu />
+    </Box>
   </header>;
 }
 
---
+++ src/Components/MiniMenu.js
@@ -0,0 +1,73 @@
+import {
+  Box,
+  FormControl, MenuItem,
+  Select,
+  SelectChangeEvent,
+} from "@mui/material";
+import {matchPath, useLocation, useNavigate} from "react-router-dom";
+import Language from "./Language";
+import {useState} from "react";
+
+export const useRouteMatch = (patterns) => {
+  const {pathname} = useLocation();
+
+  for (let i = 0; i < patterns.length; i += 1) {
+    const pattern = patterns[i];
+    const possibleMatch = matchPath(pattern, pathname);
+    if (possibleMatch !== null) {
+      return possibleMatch;
+    }
+  }
+
+  return null;
+}
+
+export const MiniMenu = () => {
+  const routeMatch = useRouteMatch(['/', '/about', '/works', '/contact']);
+  const currentTab = routeMatch?.pattern?.path ?? false;
+  const navigate = useNavigate();
+
+  const [loc, setLoc] = useState(currentTab);
+  const [open, setOpen] = useState(false);
+
+  const handleChange = (event: SelectChangeEvent) => {
+    let value = event.target.value;
+    navigate(value);
+    setLoc(value);
+  }
+
+  const handleClose = () => {
+    setOpen(false);
+  }
+
+  const handleOpen = () => {
+    setOpen(true);
+  }
+  return <nav>
+    <Box sx={{display: 'flex', alignItems: 'center'}}>
+      <Box sx={{flexGrow: 1, display: 'flex', justifyContent: 'center'}}>
+        <FormControl variant="standard">
+          <Select
+              id="location_select"
+              value={loc}
+              label="Location"
+              open={open}
+              onClose={handleClose}
+              onOpen={handleOpen}
+              onChange={handleChange}
+          >
+            <MenuItem value="/">Home</MenuItem>
+            <MenuItem value="/about">About</MenuItem>
+            <MenuItem value="/works">Works</MenuItem>
+            <MenuItem value="/contact">Contact</MenuItem>
+          </Select>
+        </FormControl>
+      </Box>
+      <Box>
+        <Language/>
+      </Box>
+    </Box>
+  </nav>
+}
+
+export default MiniMenu;
