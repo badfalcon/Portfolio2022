63482533d5be8d7ccd297ec9e0c09bedefabb9cc
badfalcon <badfalcon610@gmail.com>
2022-12-09 04:37:13 +0900

    remove i18next-http-backend

--- src/i18n.js
+++ src/i18n.js
@@ -1,11 +1,9 @@
 import i18n from "i18next";
-import Backend from 'i18next-http-backend';
 import {initReactI18next} from "react-i18next";
 import LanguageDetector from 'i18next-browser-languagedetector';
 
 
 i18n
-.use(Backend)
 .use(LanguageDetector)
 .use(initReactI18next) // passes i18n down to react-i18next
 .init({
